872cf509d67d9b0d46f4b9e6fd94180ee1f5f448
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-01-25 18:11:06 +0300

    описал структуру данных для игры

---
+++ js/game-data.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    question_type: 'single',
+    content: ['http://placehold.it/705x455'],
+    answers: 'painting',
+    description: 'Угадай, фото или рисунок?'
+  },
+  {
+    question_type: 'double',
+    content: ['http://placehold.it/468x458', 'http://placehold.it/468x458'],
+    answers: ['painting', 'photo'],
+    description: 'Угадайте для каждого изображения фото или рисунок?'
+  },
+  {
+    question_type: 'triple',
+    content: ['http://placehold.it/304x455', 'http://placehold.it/304x455', 'http://placehold.it/304x455'],
+    answers: 2,
+    description: 'Найдите рисунок среди изображений'
+  }
+];
+
+const gameParams = {
+  timer: 0,
+  lives: 3
+};
+
+const gameStats = Array.from({length: questions.length}, () => 'unknown');
+
+export {questions, gameParams, gameStats};
--- js/template-modules/rules.js
+++ js/template-modules/rules.js
@@ -21,9 +21,9 @@ const rulesTemplate = `\
   ${header}
   <div class="rules  central--none">
     <h1 class="rules__title">Правила</h1>
-    <p class="rules__description">Угадай 10 раз для каждого изображения фото <img
-      src="img/photo_icon.png" width="16" height="16"> или рисунок <img
-      src="img/paint_icon.png" width="16" height="16" alt="">.<br>
+    <p class="rules__description">Угадай 10 раз для каждого изображения фото
+      <img src="img/photo_icon.png" width="16" height="16"> или рисунок
+      <img src="img/paint_icon.png" width="16" height="16" alt="">.<br>
       Фотографиями или рисунками могут быть оба изображения.<br>
       На каждую попытку отводится 30 секунд.<br>
       Ошибиться можно не более 3 раз.<br>
