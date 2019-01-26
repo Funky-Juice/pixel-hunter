6d376e0934df6d66e3cbeddaae2e06ce1fe50178
Funky-Juice <isa86box@gmail.com>
2019-01-27 00:33:01 +0300

    10 моковых вопросов вместо 3-х

--- js/game-data.js
+++ js/game-data.js
@@ -16,6 +16,48 @@ const questions = [
     content: ['http://placehold.it/304x455', 'http://placehold.it/304x455', 'http://placehold.it/304x455'],
     answers: 2,
     description: 'Найдите рисунок среди изображений'
+  },
+  {
+    type: 'single',
+    content: ['http://placehold.it/705x455'],
+    answers: 'painting',
+    description: 'Угадай, фото или рисунок?'
+  },
+  {
+    type: 'triple',
+    content: ['http://placehold.it/304x455', 'http://placehold.it/304x455', 'http://placehold.it/304x455'],
+    answers: 2,
+    description: 'Найдите рисунок среди изображений'
+  },
+  {
+    type: 'double',
+    content: ['http://placehold.it/468x458', 'http://placehold.it/468x458'],
+    answers: ['painting', 'photo'],
+    description: 'Угадайте для каждого изображения фото или рисунок?'
+  },
+  {
+    type: 'triple',
+    content: ['http://placehold.it/304x455', 'http://placehold.it/304x455', 'http://placehold.it/304x455'],
+    answers: 2,
+    description: 'Найдите рисунок среди изображений'
+  },
+  {
+    type: 'single',
+    content: ['http://placehold.it/705x455'],
+    answers: 'painting',
+    description: 'Угадай, фото или рисунок?'
+  },
+  {
+    type: 'triple',
+    content: ['http://placehold.it/304x455', 'http://placehold.it/304x455', 'http://placehold.it/304x455'],
+    answers: 2,
+    description: 'Найдите рисунок среди изображений'
+  },
+  {
+    type: 'single',
+    content: ['http://placehold.it/705x455'],
+    answers: 'painting',
+    description: 'Угадай, фото или рисунок?'
   }
 ];
 
